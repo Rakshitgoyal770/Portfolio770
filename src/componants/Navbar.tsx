a8a890302337156a0cf02bd19ede7fd9b3dfa64a
rakshitgoyal770 <rakshitgoyal770@gmail.com>
2026-03-01 17:50:12 +0530

    projects

--- src/componants/Navbar.tsx
+++ src/componants/Navbar.tsx
@@ -5,8 +5,8 @@ export function Navbar() {
     <nav>
       <ul className="flex space-x-4 font-black">
         <li><Link to="/" className="text-black">Home</Link></li>
-        <li><Link to="/projects" className="text-black">Projects</Link></li>
         <li><Link to="/about" className="text-black">About</Link></li>
+        <li><Link to="/projects" className="text-black">Projects</Link></li>
         <li><Link to="/contact" className="text-black">Contact</Link></li>
       </ul>
     </nav>
