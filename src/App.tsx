81c03b531d927053d7674bc26e346f3afc2e8bf6
rakshitgoyal770 <rakshitgoyal770@gmail.com>
2026-03-01 16:15:30 +0530

    pagination added

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,12 @@ export default function App() {
         </div>
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Projects" element={<Projects />} />
-          <Route path="/Contact" element={<Contact />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
         </Routes>
+        
 
       </div>
     </Router>
